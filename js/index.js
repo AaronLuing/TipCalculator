f35d830c870f76720d2cb63090b43526bfc15070
Aaron Luing <aaronluing@Aarons-MacBook-Air.local>
2021-06-28 03:54:51 -0500

    the html form now properly runs the percentage calculation, and the function prevents the page reload so that the tip will stay on screen

--- js/index.js
+++ js/index.js
@@ -1,5 +1,33 @@
-let percentage = .15;
-let tip = bill => bill * percentage;
+// let percentage = document.getElementById("percent") / 100;
+// let bill = document.getElementById("bill");
+// let tip = bill => {
+//   bill * percentage;
+//   console.log("i made it!")
+//   console.log(tip)
+//   document.getElementById("yourTip") = "$" + tip;
+// }
+// let tip = bill => {
+//   bill * percentage;
+// }
 
-console.log(tip(40))
-console.log(tip(100))
+// console.log(tip(40))
+// console.log(tip(100))
+
+function calculateTip() {
+  // console.log("the start")
+  // console.log("hello?")
+  let bill = document.getElementById("bill").value;
+  // console.log(bill)
+  let percentage = document.getElementById("percent").value / 100;
+  // console.log(percentage)
+  let tip = bill * percentage;
+  // console.log(tip)
+  tip = tip.toFixed(2);
+  // document.getElementById("yourTip").innerHTML = "$" + tip;
+  // document.getElementById("totalTip").style.display = "block";
+  // document.getElementById("yourTip").innerHTML = tip;
+  // console.log(tip)
+  // console.log("the end");
+  document.getElementById("yourTip").innerHTML = tip;
+  event.preventDefault();
+}
